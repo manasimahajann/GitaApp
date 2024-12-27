dfe8ec942333b3fdbdfe2387a51ae2972f9ccd26
Manasi Mahajan <manasi@paymentsolutionsinc.net>
2024-12-27 14:53:32 -0500

    asdf

--- src/Components/Gita.jsx
+++ src/Components/Gita.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
 import chapters from "../assets/chapers.json"
-import Shlokas from "./Shlokas"
+// import Shlokas from "./Shlokas"
 import LangauageBtns from "./LangauageBtns"
 const Gita = () => {
 	const [selectedChapter, setSelectedChapter] = useState(null)
@@ -17,30 +17,30 @@ const Gita = () => {
 	return (
 		<div className="flex flex-col items-center">
 			<LangauageBtns onLanguageChange={handleLanguageChange} />
-			{selectedChapter === null ? (
-				chapters.map((chapter, i) => (
-					<p
-						key={i}
-						className="w-full max-w-xs h-auto m-1 text-orange-500 bg-white border border-gray-300 shadow-sm rounded-lg text-left px-4 py-2 cursor-pointer transition-transform transform hover:scale-105 hover:bg-gray-50 overflow-hidden"
-						onClick={() => handleChapterClick(i)}
-						style={{
-							whiteSpace: "nowrap",
-							textOverflow: "ellipsis",
-							overflow: "hidden",
-						}}
-					>
-						<span className="font-semibold">{chapter.chapter_number}</span> -{" "}
-						{selectedLanguage === "Sanskrit"
-							? chapter.name
-							: chapter.name_transliterated}
-					</p>
-				))
-			) : (
-				<Shlokas
+			{selectedChapter === null
+				? chapters.map((chapter, i) => (
+						<p
+							key={i}
+							className="w-full max-w-xs h-auto m-1 text-orange-500 bg-white border border-gray-300 shadow-sm rounded-lg text-left px-4 py-2 cursor-pointer transition-transform transform hover:scale-105 hover:bg-gray-50 overflow-hidden"
+							onClick={() => handleChapterClick(i)}
+							style={{
+								whiteSpace: "nowrap",
+								textOverflow: "ellipsis",
+								overflow: "hidden",
+							}}
+						>
+							<span className="font-semibold">{chapter.chapter_number}</span> -{" "}
+							{selectedLanguage === "Sanskrit"
+								? chapter.name
+								: chapter.name_transliterated}
+						</p>
+				  ))
+				: {
+						/* <Shlokas
 					chapter={selectedChapter}
 					selectedLanguage={selectedLanguage}
-				/>
-			)}
+				/> */
+				  }}
 		</div>
 	)
 }
